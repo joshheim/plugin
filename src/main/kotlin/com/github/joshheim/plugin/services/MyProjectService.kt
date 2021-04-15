3b9a5436381754c6f69d20a1eda54f22eca91a38
GitHub Action <action@github.com>
2021-04-15 12:32:30 +0000

    Template cleanup

--- src/main/kotlin/com/github/joshheim/plugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/joshheim/plugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.joshheim.plugin.services
 
+import com.github.joshheim.plugin.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
